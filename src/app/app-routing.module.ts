ca1e502a357a1821ce32b8f9c0f975ef411a8c62
kbbansi <kwabenaampofo5@gmail.com>
2022-12-28 17:12:31 +0000

    add page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DefaultLayoutComponent } from './default-layout/default-layout.component';
+import { DefaultLayoutComponent } from './core/default-layout/default-layout.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -25,6 +26,10 @@ const routes: Routes = [
     path: '',
     component: DefaultLayoutComponent,
     children: []
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
